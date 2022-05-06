490f5fcded052677e2572a57d7edb446bd070430
Florian <92161528+peflor@users.noreply.github.com>
2022-05-06 16:19:17 +0200

    Echauffement Liens GET exo 1

---
+++ page1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cerulean/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP FORM CRUD</title>
+</head>
+<body>
+    <h1>Quel est ton régime alimentaire ?</h1>
+    <br>
+    <a href="page2.php?name=vegan">Je suis Vegan</a>
+    <br>
+    <br>
+    <a href="page2.php?name=carni">Je suis Carnivore</a>
+
+</body>
+</html>
---
+++ page2.php
@@ -0,0 +1,9 @@
+<?php
+
+// var_dump($_GET);
+
+if($_GET['name'] == "vegan") {
+    echo "toi tu Kiffes vraiment la salade !";
+}else{
+    echo "Tu m'étonnes un bon steak ça déchire";
+}
